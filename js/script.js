3619861c7df15110c0d4db7c9ef0c85eb62a5b36
moretti-matteo <matteomoretti2000@hotmail.it>
2022-04-28 16:32:28 +0200

    modifiche JS

--- js/script.js
+++ js/script.js
@@ -5,17 +5,21 @@ do {
     km = Number(prompt("how many kilometers do you want to do?"));
     if (isNaN(km)) {
         alert("puoi inserire solo numeri, Riprova")
+    } else if (km <= 0) {
+        alert("Puoi inserire solo numeri maggiori di 0");
     }
 
-} while (isNaN(km));
+} while (isNaN(km) || km <= 0);
 
 do {
     age = Number(prompt("How old are you?"));
     if (isNaN(age)) {
         alert("puoi inserire solo numeri, Riprova")
+    } else if (age <= 0) {
+        alert("Puoi inserire solo numeri maggiori di 0");
     }
 
-} while (isNaN(age));
+} while (isNaN(age) || age <= 0);
 
 const ticketPrice = km * 0.21;
 let discount = 0;
@@ -30,7 +34,7 @@ const finalPriceTicket = ticketPrice - discount;
 
 document.getElementById("ticketPrice").innerHTML = `${finalPriceTicket.toFixed(2)}€`;
 
-if(discount > 0){
+if (discount > 0) {
     document.getElementById("line").classList.remove("hidden");
     document.getElementById("initialPrice").innerHTML = `Initial price: ${ticketPrice.toFixed(2)}€`;
     document.getElementById("discount").innerHTML = `Discount applied: -${discount.toFixed(2)}€`;
